7eb7807f53912fefbc415de3524d68bfbdfeb77f
Pohorielova <anna.pogorielova@gmail.com>
2023-01-04 15:48:23 +0100

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,29 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Gallery } from './ImageGallery.styled';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import PropTypes from 'prop-types';
-class ImageGallery extends Component {
-  render() {
-    return (
-      <Gallery>
-        {this.props.data.map(
-          ({ webformatURL, id, tags, largeImageURL }, index) => (
-            <ImageGalleryItem
-              webformatURL={webformatURL}
-              key={id}
-              index={index}
-              tags={tags}
-              largeImageURL={largeImageURL}
-              modalFn={this.props.modalFn}
-            ></ImageGalleryItem>
-          )
-        )}
-      </Gallery>
-    );
-  }
+export default function ImageGallery({ data, modalFn }) {
+  return (
+    <Gallery>
+      {data.map(({ webformatURL, id, tags, largeImageURL }, index) => (
+        <ImageGalleryItem
+          webformatURL={webformatURL}
+          key={id}
+          index={index}
+          tags={tags}
+          largeImageURL={largeImageURL}
+          modalFn={modalFn}
+        ></ImageGalleryItem>
+      ))}
+    </Gallery>
+  );
 }
 
-export default ImageGallery;
 ImageGallery.propTypes = {
   searchQuery: PropTypes.string.isRequired,
   page: PropTypes.number.isRequired,
